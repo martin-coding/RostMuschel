34ec3d3012eb756d6b3e59156f53326d46cbcb0c
Smirnov-Maximilian <128090922+Smirnov-Maximilian@users.noreply.github.com>
2025-02-03 21:16:34 +0100

    feat: add exit command

--- RostMuschel/src/main.rs
+++ RostMuschel/src/main.rs
@@ -1,5 +1,6 @@
 use std::io;
 use std::io::Write;
+use std::str::SplitWhitespace;
 
 // The character that is printed on every new line.
 const SHELL_CHAR: char = '>';
@@ -8,7 +9,7 @@ fn main() {
     loop {
         // Prints the shell character and a whitespace.
         print!("{} ", SHELL_CHAR);
-        io::stdout().flush().unwrap();
+        flush_stdout();
 
         // Get user input.
         let mut input = String::new();
@@ -16,7 +17,47 @@ fn main() {
             .read_line(&mut input)
             .expect("Failed to read line.");
 
-        // TODO: logic
-        println!("{}: command not found", input.trim());
+        // Split the input into a command and parameters.
+        let mut split: SplitWhitespace = input.trim().split_whitespace();
+        let command: &str = split.next().unwrap_or_default();
+        let parameters: Vec<&str> = split.collect();
+
+        // Handle functionality and output.
+        handle_command(command, parameters.as_slice());
+        flush_stdout();
+    }
+}
+
+/// Flushes the output buffer of stdout.
+fn flush_stdout() {
+    io::stdout().flush().expect("Failed to flush stdout.");
+}
+
+/// Handles command logic and output.
+fn handle_command(command: &str, parameters: &[&str]) {
+    match command {
+        "exit" => {
+            // Check if correct number of parameters provided.
+            if parameters.len() == 0 {
+                println!("{}: no parameter provided", command);
+                return;
+            } else if parameters.len() > 1 {
+                println!("{}: too many parameters provided", command);
+                return;
+            }
+
+            // Parse the parameter to an integer.
+            let exit_value: i32 = match parameters[0].parse() {
+                Ok(num) => num,
+                Err(_) => {
+                    println!("{}: must provide a number", command);
+                    return;
+                },
+            };
+
+            // Now exit with provided exit value.
+            std::process::exit(exit_value)
+        },
+        _ => println!("{}: command not found", command),
     }
 }
