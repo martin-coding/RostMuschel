ae24016a1b0a0f92ed5dd64a55f5a8b0ba1ad129
Smirnov-Maximilian <128090922+Smirnov-Maximilian@users.noreply.github.com>
2025-02-04 19:28:31 +0100

    refactor: seperate code into multiple files

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
+mod builtins;
+mod call;
+
+use crate::builtins::*;
+use crate::call::Call;
+
 use std::io::{self, Write};
-use std::str::SplitWhitespace;
 
 // The character that is printed on every new line.
 const SHELL_PROMPT: char = '>';
@@ -24,23 +29,6 @@ fn main() {
     }
 }
 
-/// Represent a command call with arguments.
-struct Call<'a> {
-    command: &'a str,
-    arguments: Vec<&'a str>,
-}
-
-impl<'a> Call<'a> {
-    /// Turns an input string into a command call.
-    fn from_input(input: &'a str) -> Self {
-        // Split the input into a command and arguments.
-        let mut split: SplitWhitespace = input.split_whitespace();
-        let command: &str = split.next().unwrap_or_default();
-        let arguments: Vec<&str> = split.collect();
-        Call { command, arguments }
-    }
-}
-
 /// Flushes the output buffer of stdout.
 fn flush_stdout() {
     io::stdout().flush().expect("Failed to flush stdout.");
@@ -49,29 +37,8 @@ fn flush_stdout() {
 /// Handles command logic and output.
 fn handle_command(call: &Call) {
     match call.command {
-        "exit" => {
-            // Check if correct number of arguments provided.
-            if call.arguments.is_empty() {
-                // Always exit without error value if no argument provided.
-                std::process::exit(0)
-            } else if call.arguments.len() > 1 {
-                println!("{}: too many arguments provided", call.command);
-                return;
-            }
-
-            // Parse the argument to an integer.
-            let exit_value: i32 = match call.arguments[0].parse() {
-                Ok(num) => num,
-                Err(_) => {
-                    println!("{}: must provide a number", call.command);
-                    return;
-                }
-            };
-
-            // Now exit with provided exit value.
-            std::process::exit(exit_value)
-        }
-        "echo" => println!("{}", call.arguments.join(" ")),
-        _ => println!("{}: command not found", call.command),
+        "exit" => exit::call(call),
+        "echo" => echo::call(call),
+        _ => unknown::call(call),
     }
 }
