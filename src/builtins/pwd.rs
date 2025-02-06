625dd886e20f1ed715b64274d970245265f8fa1c
Smirnov-Maximilian <128090922+Smirnov-Maximilian@users.noreply.github.com>
2025-02-06 21:17:43 +0100

    refactor: change to argument

--- src/builtins/pwd.rs
+++ src/builtins/pwd.rs
@@ -4,7 +4,7 @@ use std::env;
 
 /// Prints the current working directory.
 pub fn call(_call: &Call) {
-    // Ignores all parameters
+    // Ignores all arguments
 
     let current_dir = env::current_dir().expect("Failed to get current working directory.");
     println!("{}", current_dir.display());
