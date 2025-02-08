b023fe7cddb91d4b5328b89b15da610253c6f274
Smirnov-Maximilian <128090922+Smirnov-Maximilian@users.noreply.github.com>
2025-02-08 19:33:18 +0100

    fix: add windows support for non-builtin command execution

--- src/builtins/unknown.rs
+++ src/builtins/unknown.rs
@@ -17,7 +17,13 @@ pub fn call(call: &Call) {
 
     // Replace ~ or ~/ at the beginning with $HOME, because Path::canonicalize() will not do it.
     if call.command == "~" || call.command.starts_with("~/") {
-        command = env::var("HOME").expect("Failed to get HOME variable.");
+        command = match get_home_dir() {
+            Some(home_dir) => home_dir,
+            None => {
+                eprintln!("Failed to get home directory.");
+                return;
+            }
+        };
         command.push_str(&call.command[1..]);
     } else {
         command = call.command.to_owned();
@@ -33,8 +39,21 @@ pub fn call(call: &Call) {
         }
     }
 
+    // Initialise the command depending on the platform.
+    // For Windows invoking the command via the PowerShell is necessary, otherwise it is not found.
+    let mut child: Command;
+    if cfg!(target_family = "unix") {
+        child = Command::new(&command);
+    } else if cfg!(target_family = "windows") {
+        child = Command::new("PowerShell.exe");
+        child.arg("-Command").arg(&command);
+    } else {
+        eprintln!("Unsupported platform");
+        return;
+    };
+
     // Spawn a child process for the given command.
-    let mut child = match Command::new(&command).args(&call.arguments).spawn() {
+    let mut child = match child.args(&call.arguments).spawn() {
         Ok(child) => child,
         Err(err) => {
             // Match used instead of only .kind().to_string() to change a few default descriptions
@@ -53,4 +72,16 @@ pub fn call(call: &Call) {
 
     // Wait for child to finish execution.
     child.wait().expect("Failed to wait on child");
+}
+
+/// Returns the path to the home directory as a String if possible.
+fn get_home_dir() -> Option<String> {
+    #[allow(deprecated)]
+    // home_dir() only deprecated for Windows (may lead to unexpected behaviour on Windows)
+    let home_dir = match env::home_dir() {
+        Some(home_dir) => home_dir.display().to_string(),
+        None => return None,
+    };
+    #[warn(deprecated)]
+    Some(home_dir)
 }
